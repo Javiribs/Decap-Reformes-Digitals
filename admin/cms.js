40015d1126a89141dfb25c2ab185895e3a020db6
Javi Ribas <javiribs@gmail.com>
2025-09-04 14:29:53 +0200

    delete css

--- admin/cms.js
+++
@@ -1,4 +0,0 @@
-// Se carga después de decap-cms.js en index.html
-CMS.registerPreviewStyle('/reset.css');
-CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewStyle('/styles-cookies.css'); // si quieres que las legales se vean igual
