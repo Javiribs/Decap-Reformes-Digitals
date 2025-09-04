02dd0642cd705c474e72cb6abd6faff65a47d7ca
Javi Ribas <javiribs@gmail.com>
2025-09-04 14:07:27 +0200

    css style to admin

---
+++ admin/cms.js
@@ -0,0 +1,4 @@
+// Se carga después de decap-cms.js en index.html
+CMS.registerPreviewStyle('/reset.css');
+CMS.registerPreviewStyle('/styles.css');
+CMS.registerPreviewStyle('/styles-cookies.css'); // si quieres que las legales se vean igual
